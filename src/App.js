febaf79067b2119461a614562648c13708a9acd3
Kent Yucra <mckkentmwk@gmail.com>
2022-10-26 12:06:25 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import HeaderBar from "./components/HeaderBar"
-import ExampleCard from "./components/ExampleCard"
-import DenseTable from './components/DemoTable';
+import HeaderBar from './components/HeaderBar';
+import AppRouter from './components/AppRouter';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <HeaderBar />
-      <div style={{ padding: 10 }}>
-        <DenseTable />
-        <ExampleCard />
-        <ExampleCard />
-        <ExampleCard />
-        <ExampleCard />
-      </div>
+      <AppRouter />
     </div>
   );
 }
